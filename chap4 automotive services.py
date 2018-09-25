8e73628cbe548f3cd739062371d78741cb930f58
mirandarcarlos <43382385+mirandarcarlos@users.noreply.github.com>
2018-09-25 10:59:51 -0500

    Update chap4 automotive services.py

--- chap4 automotive services.py
+++ chap4 automotive services.py
@@ -1,3 +1,16 @@
-print("Davy's auto shop services\n")
-print('Oil change -- $35\n Tire rotation -- $19\n')
-    
+shop_services = {
+    'Oil change' : 35, 
+    'Tire rotation': 19, 
+    'Car wash': 7, 
+    'Car wax' : 12
+ }
+print('Davy\'s auto shop services')
+print('Oil Change -- $%s' % shop_services['Oil change'])
+print('Tire Rotation -- $%s' % shop_services['Tire rotation'])
+print('Car Wash -- $%s' % shop_services['Car wash'])
+print('Car Wax -- $%s' % shop_services['Car wax'])
+print()
+
+first_serv = input('Select first service:\n')
+
+second_serv = input('Select second service:\n')    
